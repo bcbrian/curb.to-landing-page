b660d664cb54455e3019b6f6147cc450874ae247
Brian Bartholomew <me@bcbrian.com>
2020-04-20 20:44:14 -0700

    changed words

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Curb To</h1>
+      <h2>Landing Page</h2>
     </div>
   );
 }
